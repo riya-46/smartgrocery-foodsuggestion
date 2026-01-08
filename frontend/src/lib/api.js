b08dd27be5dcb9bea228f71c422e7cbea40ab9c6
riya-46 <riyarani632004@gmail.com>
2026-01-08 21:26:09 +0530

    backend URL setup to api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL: "https://smartgrocery-backend.onrender.com",
 });
 
 export default api;
